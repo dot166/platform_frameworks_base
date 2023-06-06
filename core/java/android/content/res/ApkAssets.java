b073efeeaf55fbe5f4f630474a45fa959448a189
GrapheneOS <contact@grapheneos.org>
2023-06-06 14:44:05 +0300

    gmscompat: add compatibility layer for unprivileged GMS
    Originally authored by Danny Lin <danny@kdrag0n.dev> for inclusion in
    GrapheneOS. It has since been substantially extended and partially
    rewritten by Dmitry Muhomor <muhomor.dmitry@gmail.com> (primarily) and
    Daniel Micay <danielmicay@gmail.com>.
    
    Commit history:
    (better viewed with git log --grep gmscompat $TAG_NAME)
    
    August 2024 - November 2024:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024112700
    
    June 2024 - August 2024:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024083100
    
    March 2024 - June 2024:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024053100
    
    November 2023 - March 2024:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024030300
    
    August 2023 - November 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2023112900
    
    May 2023 - August 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2023080800
    
    February 2023 - May 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ2A.230505.002.2023052900
    
    August 2022 - February 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ1A.230205.002.2023020600
    
    June 2022 - August 2022:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.006.2022073000
    
    October 2021 - June 2022:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.002.2022053100
    
    July 2021 - October 2021:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/RQ3A.211001.001.2021100606

--- core/java/android/content/res/ApkAssets.java
+++ core/java/android/content/res/ApkAssets.java
@@ -27,6 +27,7 @@ import android.ravenwood.annotation.RavenwoodKeepWholeClass;
 import android.text.TextUtils;
 
 import com.android.internal.annotations.GuardedBy;
+import com.android.internal.gmscompat.dynamite.GmsDynamiteClientHooks;
 
 import dalvik.annotation.optimization.CriticalNative;
 
@@ -155,7 +156,7 @@ public final class ApkAssets {
      */
     public static @NonNull ApkAssets loadFromPath(@NonNull String path, @PropertyFlags int flags)
             throws IOException {
-        return new ApkAssets(FORMAT_APK, path, flags, null /* assets */);
+        return loadFromPath(path, flags, null);
     }
 
     /**
@@ -169,6 +170,13 @@ public final class ApkAssets {
      */
     public static @NonNull ApkAssets loadFromPath(@NonNull String path, @PropertyFlags int flags,
             @Nullable AssetsProvider assets) throws IOException {
+        if (GmsDynamiteClientHooks.enabled()) {
+            ApkAssets apkAssets = GmsDynamiteClientHooks.loadAssetsFromPath(path, flags, assets);
+            if (apkAssets != null) {
+                return apkAssets;
+            }
+        }
+
         return new ApkAssets(FORMAT_APK, path, flags, assets);
     }
 
