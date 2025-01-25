eaec101a574746a9b025252edad29db6c7860bd1
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-01-25 13:53:13 +0200

    gmscompat: remove manual notification perm pre-grant for GmsCompatApp
    Notification pregrant is now made by adding a permission pregrant XML in GmsCompat app repo.

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -66,7 +66,6 @@ import android.util.SparseArray;
 import android.util.Xml;
 
 import com.android.internal.R;
-import com.android.internal.gmscompat.GmsCompatApp;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.XmlUtils;
 import com.android.modules.utils.TypedXmlPullParser;
@@ -835,7 +834,6 @@ final class DefaultPermissionGrantPolicy {
                         ACTIVITY_RECOGNITION_PERMISSIONS);
             }
         }
-        grantPermissionsToSystemPackage(pm, GmsCompatApp.PKG_NAME, userId, NOTIFICATION_PERMISSIONS);
 
         // Music
         Intent musicIntent = new Intent(Intent.ACTION_VIEW)
