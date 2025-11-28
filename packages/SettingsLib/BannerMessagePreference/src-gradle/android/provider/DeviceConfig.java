f071a1600b5d4db77ec3ba863c30d0877e054ac2
._______166 <retromac9x2@gmail.com>
2025-11-28 20:40:31 +0000

    add gradle for jLib using SettingsLib

---
+++ packages/SettingsLib/BannerMessagePreference/src-gradle/android/provider/DeviceConfig.java
@@ -0,0 +1,9 @@
+package android.provider;
+
+import androidx.annotation.NonNull;
+
+public class DeviceConfig {
+    public static long getLong(@NonNull String namespace, @NonNull String name, long defaultValue) {
+        return defaultValue;
+    }
+}
