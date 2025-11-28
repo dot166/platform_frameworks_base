05d255e5821a79cc631749a429920e07a6eaab87
._______166 <retromac9x2@gmail.com>
2025-11-28 20:40:31 +0000

    add gradle for jLib using SettingsLib

---
+++ packages/SettingsLib/Metadata/src-gradle/android/provider/SearchIndexablesProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.provider;
+
+import androidx.annotation.Nullable;
+
+import android.content.ContentProvider;
+import android.database.Cursor;
+
+public abstract class SearchIndexablesProvider extends ContentProvider {
+    public abstract Cursor queryRawData(String[] projection);
+    public abstract Cursor queryNonIndexableKeys(String[] projection);
+    @Nullable
+    public abstract Cursor queryDynamicRawData(@Nullable String[] projection);
+}
