8d739e7d56399008794579bcca97615c63105e63
._______166 <retromac9x2@gmail.com>
2026-03-31 12:27:29 +0100

    fix URLs on maven

--- packages/SettingsLib/SelectorWithWidgetPreference/build.gradle.kts
+++ packages/SettingsLib/SelectorWithWidgetPreference/build.gradle.kts
@@ -51,7 +51,7 @@ mavenPublishing {
         name = nameVal
         description = "SettingsLib from GrapheneOS"
         inceptionYear = "2025"
-        url = "https://github.com/dot166/platform_frameworks_base/tree/16-qpr1/packages/SettingsLib"
+        url = "https://github.com/dot166/platform_frameworks_base/tree/16-qpr2/packages/SettingsLib"
         licenses {
             license {
                 name.set("Apache License")
