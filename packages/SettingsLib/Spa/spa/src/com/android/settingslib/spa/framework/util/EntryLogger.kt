636db6d4151f7b9f97e079c71ca63031cca99338
._______166 <retromac9x2@gmail.com>
2026-06-15 10:01:20 +0100

    create SpinnerPreference

--- packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/framework/util/EntryLogger.kt
+++ packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/framework/util/EntryLogger.kt
@@ -19,6 +19,7 @@ package com.android.settingslib.spa.framework.util
 import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.core.os.bundleOf
+import com.android.settingslib.spa.framework.common.LOG_DATA_SET_STATUS
 import com.android.settingslib.spa.framework.common.LOG_DATA_SWITCH_STATUS
 import com.android.settingslib.spa.framework.common.LocalEntryDataProvider
 import com.android.settingslib.spa.framework.common.LogCategory
@@ -55,5 +56,15 @@ fun wrapOnSwitchWithLog(onSwitch: ((checked: Boolean) -> Unit)?): ((checked: Boo
     return {
         logEvent(LogEvent.ENTRY_SWITCH, bundleOf(LOG_DATA_SWITCH_STATUS to it))
         onSwitch(it)
+    }
+}
+
+@Composable
+fun wrapOnSetItemIdWithLog(onSetItemId: ((itemId: Int) -> Unit)?): ((itemId: Int) -> Unit) {
+    if (onSetItemId == null) return {} // has the same effect as returning null like the others, but spinner does not like getting a null value, so return a stub
+    val logEvent = logEntryEvent()
+    return {
+        logEvent(LogEvent.ENTRY_SET, bundleOf(LOG_DATA_SET_STATUS to it))
+        onSetItemId(it)
     }
 }
